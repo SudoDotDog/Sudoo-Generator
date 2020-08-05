8256d3e905f04d40cc7e10dfe79b215be9e776f7
WMXPY <wm@mengw.io>
2020-08-05 13:10:19 -0500

    add number related generation

--- src/base.ts
+++ src/base.ts
@@ -5,9 +5,11 @@
  * @description Base
  */
 
-import { StringPattern } from "@sudoo/pattern";
+import { NumberPattern, StringPattern, BigIntPattern } from "@sudoo/pattern";
 import { randomIntegerBetween, randomString } from "@sudoo/random";
 import { GenerateFunction, GenerateOption, StackElement } from "./declare";
+
+declare const BigInt: any;
 
 export const generateStringPattern: GenerateFunction<StringPattern> = (
     pattern: StringPattern,
@@ -20,6 +22,38 @@ export const generateStringPattern: GenerateFunction<StringPattern> = (
 
     const length: number = randomIntegerBetween(min, max);
     const result: string = randomString(length);
+
+    return result;
+};
+
+export const generateNumberPattern: GenerateFunction<NumberPattern> = (
+    pattern: NumberPattern,
+    option: GenerateOption,
+    stack: StackElement[],
+): number => {
+
+    const min: number = pattern.minimum ?? 0;
+    const max: number = pattern.maximum ?? 2048;
+
+    const result: number = randomIntegerBetween(min, max);
+
+    return result;
+};
+
+export const generateBigIntPattern: GenerateFunction<BigIntPattern> = (
+    pattern: BigIntPattern,
+    option: GenerateOption,
+    stack: StackElement[],
+): bigint => {
+
+    const min: number = pattern.minimum
+        ? Number(pattern.minimum)
+        : 0;
+    const max: number = pattern.maximum
+        ? Number(pattern.maximum)
+        : 2048;
+
+    const result: bigint = BigInt(randomIntegerBetween(min, max));
 
     return result;
 };
